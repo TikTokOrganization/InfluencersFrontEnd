d8fc242b08a76350589c959fc56b322c94f4d615
unknown <songk6688@gmail.com>
2023-02-02 16:59:32 -0500

    added basic containers

--- influencers-front-end/src/App.js
+++ influencers-front-end/src/App.js
@@ -2,22 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Youtube Shorts Sorter
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className="App-container">
+        <div className="App-left">
+        </div>
+        <div className="App-right">
+        </div>
+      </div>
     </div>
   );
 }
