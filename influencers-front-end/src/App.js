6a53dde39afcae8ca2e1248f8c992024e5b2a27f
unknown <songk6688@gmail.com>
2023-02-14 16:09:56 -0500

    connected category list to send selected category up

--- influencers-front-end/src/App.js
+++ influencers-front-end/src/App.js
@@ -1,25 +1,27 @@
 import './App.css';
 import CategoryList from './Components/CategoryList/CategoryList.js'
+import React, { useEffect, useState } from "react";
 
-function App() {    
-  return (
-    <div className="App">
-      <div className="App-header">
-        <p>
-          Youtube Shorts Sorter
-        </p>
-      </div>
+function App() {
+    const [selectedCategory, setSelectedCategory] = useState(-1);
 
-      <div className="App-container">
-        <div className="App-left">
-            <CategoryList>
-            </CategoryList>
+    return (
+        <div className="App">
+        <div className="App-header">
+            <p>
+            Youtube Shorts Sorter
+            </p>
         </div>
-        <div className="App-right">
+
+        <div className="App-container">
+            <div className="App-left">
+                <CategoryList selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}></CategoryList>
+            </div>
+            <div className="App-right">
+            </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
--- influencers-front-end/src/Components/CategoryList/CategoryList.js
+++ influencers-front-end/src/Components/CategoryList/CategoryList.js
@@ -1,27 +1,50 @@
 import './CategoryList.css';
+import React, { useEffect, useState } from "react";
 
-function CategoryList() {
+function CategoryList(props) {
     const categories = ["Dog", "Cat"];
-    
+    let category = [categories.length];
+    let [selectedCategory, setSelectedCategory] = useState(props.selectedCategory);
+
+    // function handleCategoryChange(event) {
+    //     props.onChange(event.target.value);
+    // }
+
     function addCategories() {
         var toAdd = document.createDocumentFragment();
         for(var i = 0; i < categories.length; i++) {
-            console.log(i);
-            var category = document.createElement("div");
-            category.id = "category-" + i;
-            category.className = "category";
-            category.textContent = categories[i];
-            toAdd.appendChild(category);
+            category[i] = document.createElement("div");
+            category[i].id = i;
+            category[i].className = "category";
+            category[i].textContent = categories[i];
+            category[i].onclick = (function(i) {
+                return function() {
+                    if (selectedCategory == i) {
+                        selectedCategory = -1;
+                    } else {
+                        selectedCategory = i;
+                    }
+                    for (const c of category) {
+                        if (parseInt(c.id) == selectedCategory) {
+                            c.style.backgroundColor = "#ADD8E6";
+                        } else {
+                            c.style.backgroundColor = "white";
+                        }
+                    }
+                    props.setSelectedCategory(selectedCategory);
+                };
+            })(i);
+            toAdd.appendChild(category[i]);
         }
         document.getElementById("categoryList").appendChild(toAdd);
     }
-    
+
     window.onload = function() {
         addCategories();
     };
 
     return(
-        <div id = "categoryList">
+        <div id="categoryList">
         </div>
     );
 }
