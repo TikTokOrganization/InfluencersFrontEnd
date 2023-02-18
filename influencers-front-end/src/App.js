9e93c29026fe40e644a8ba98c79aca24111b3eb4
miquaz <4180687+miquaz@users.noreply.github.com>
2023-02-18 10:17:35 -0500

    Working on video pane
    Unsuccessfully trying to use flexbox to align the video thumbnails. Found a solid example online which wraps the images as list items which I am now going to try.

--- influencers-front-end/src/App.js
+++ influencers-front-end/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import CategoryList from './Components/CategoryList/CategoryList.js'
+import ShortExample from './Images/ShortExample.png';
 
 function App() {    
   return (
@@ -16,10 +17,68 @@ function App() {
             </CategoryList>
         </div>
         <div className="App-right">
+          <div className="Video-row">
+            <div className="Video-one">
+            <img className="Video"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            </div>
+            <div className="Video-two">
+            <img className="Video"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            </div>
+            <div className="Video-three">
+            <img className="Video"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            </div>
+            <div className="Video-four">
+            <img className="Video"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            </div>
+            <div className="Video-five">
+            <img className="Video"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            </div>
+          </div>
+          <div className="Video-row">
+            <img className="Video"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+          </div>
         </div>
       </div>
     </div>
   );
+
+/*
+
+          <div className="Video-row">
+            <div className="Video-one">
+            <img className="Video-one"
+              src={"Images/ShortExample.png"}
+              alt={"ShortExample"}
+            />
+            </div>
+            <div className="Video-two">
+            <img className="Video-two"
+              src={"Images/ShortExample.png"}
+              alt={"ShortExample"}
+            />
+            </div>
+          </div>
+
+*/
+
 }
 
 export default App;
