28cacbe8675bf748007160607f2343138b16891d
unknown <songk6688@gmail.com>
2023-02-19 16:17:59 -0500

    added data json, and created data structure with 2 shorts for MVP

--- influencers-front-end/src/App.js
+++ influencers-front-end/src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 import CategoryList from './Components/CategoryList/CategoryList.js'
 import React, { useEffect, useState } from "react";
+import finalOutput from './finalOutput.json';
 
 function App() {
     const [selectedCategory, setSelectedCategory] = useState(-1);
+
+    let categories;
+    // 2 shorts to fill "data". data[0] is a dog short, data[1] is a cat short
+    let data = [finalOutput["items"][0], finalOutput["items"][0]];
+
+    console.log(data);
+
+    function getCategories() {
+        // TODO: this function gets categories based on the tags of the videos
+        // right now, it is "autofilled" based off of "data"
+        categories = ["Dog", "Cat"]
+    }
 
     return (
         <div className="App">
