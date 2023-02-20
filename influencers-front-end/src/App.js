f143889043fec08f371d0d92386da49f2f2b1c6b
miquaz <4180687+miquaz@users.noreply.github.com>
2023-02-19 19:04:25 -0500

    Created VideoPane component
    Moved VideoPane content into its own component.

--- influencers-front-end/src/App.js
+++ influencers-front-end/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import CategoryList from './Components/CategoryList/CategoryList.js'
+import VideoPane from './Components/VideoPane/VideoPane.js'
 import ShortExample from './Images/ShortExample.png';
 
 function App() {    
@@ -17,86 +18,8 @@ function App() {
             </CategoryList>
         </div>
         <div className="App-right">
-          <div className="Video-row">
-            <iframe className="Video-one"
-              src="https://www.youtube.com/embed/EhKetUBuyxc"
-              title="that girl era / Monday dinner #healrhylifestyle #foodie #shorts"
-              frameborder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              allowfullscreen>
-            </iframe>
-            <img className="Video-one"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-two"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-three"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-four"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <img className="Video-five"
-              src={ShortExample}
-              alt={"ShortExample"}
-            />
-            <div className="Video-five"></div>
-            <div className="Video-five"></div>
-            <div className="Video-five"></div>
-          </div>
+          <VideoPane>
+          </VideoPane>
         </div>
       </div>
     </div>
---
+++ influencers-front-end/src/Components/VideoPane/VideoPane.js
@@ -0,0 +1,92 @@
+import './VideoPane.css';
+import ShortExample from '../../Images/ShortExample.png';
+
+function VideoPane() {
+
+    return(
+
+        <div className="Video-row">
+            <iframe className="Video-one"
+              src="https://www.youtube.com/embed/EhKetUBuyxc"
+              title="that girl era / Monday dinner #healrhylifestyle #foodie #shorts"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen>
+            </iframe>
+            <img className="Video-one"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-two"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-three"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-four"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <img className="Video-five"
+              src={ShortExample}
+              alt={"ShortExample"}
+            />
+            <div className="Video-five"></div>
+            <div className="Video-five"></div>
+            <div className="Video-five"></div>
+          </div>
+
+    )
+    
+}
+export default VideoPane;
