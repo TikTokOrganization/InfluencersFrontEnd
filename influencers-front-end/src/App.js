926e73bab569f8a8ae60eaa0fb00fbffc952f00d
miquaz <4180687+miquaz@users.noreply.github.com>
2023-03-29 17:53:09 -0400

    Revert "Merge branch 'front-end-setup-copy' of https://github.com/TikTokOrganization/InfluencersFrontEnd into front-end-setup-copy"
    This reverts commit 5124bb05e87a20a768e39de17bf35dc6c7d58a0b, reversing
    changes made to 3569aa21938137e2c2205725f5a1099c930cd189.

--- influencers-front-end/src/App.js
+++ influencers-front-end/src/App.js
@@ -9,16 +9,49 @@ import VideoPane from './Components/VideoPane/VideoPane.js'
 
 function App() {
     const [selectedCategory, setSelectedCategory] = useState(-1);
-    let [categories, setCategories] = useState({});
+    let [categories, setCategories] = useState({
+        "1": "Film & Animation",
+        "2": "Autos & Vehicles",
+        "10": "Music",
+        "15": "Pets & Animals",
+        "17": "Sports",
+        "18": "Short Movies",
+        "19": "Travel & Events",
+        "20": "Gaming",
+        "21": "Videoblogging",
+        "23": "Comedy",
+        "24": "Entertainment",
+        "25": "News & Politics",
+        "26": "Howto & Style",
+        "27": "Education",
+        "28": "Science & Technology",
+        "29": "Nonprofits & Activism",
+        "30": "Movies",
+        "31": "Anime/Animation",
+        "32": "Action/Adventure",
+        "33": "Classics",
+        "34": "Comedy",
+        "35": "Documentary",
+        "36": "Drama",
+        "37": "Family",
+        "38": "Foreign",
+        "39": "Horror",
+        "40": "Sci-Fi/Fantasy",
+        "41": "Thriller",
+        "42": "Shorts",
+        "43": "Shows",
+        "44": "Trailers"
+    })
+    // 2 shorts to fill "data". data[0] is a dog short, data[1] is a cat short
+    let data = [finalOutput["items"][0], finalOutput["items"][0]];
+
     
     useEffect(() => {
-        fetch("http://localhost:8080/getShortsOfCategory")
+        fetch("http://localhost:8080/getCategories")
             .then(res => res.json())
-            .then((response) => {
-                console.log(JSON.stringify(response));
-                setCategories(response);
-            })
+            .then(data => console.log(data));
     }, [])
+
 
     return (
         <div className="App">
@@ -31,7 +64,6 @@ function App() {
         <div className="App-container">
             <div className="App-left">
                 <CategoryList categories={categories} setCategories={setCategories} selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}></CategoryList>
-                {/*
                 <div className="AddButtonContainer">
                     <AddButton categories={categories} setCategories={setCategories}></AddButton >
                 </div>
@@ -41,13 +73,13 @@ function App() {
                 <div className="DeleteButtonContainer">
                     <DeleteButton></DeleteButton>
                 </div>
-                */}
             </div>
             <div className="App-right">
-              {<VideoPane
+              <VideoPane
                 selectedCategory={selectedCategory}
-                data={finalOutput}>
-            </VideoPane>}
+                data={finalOutput}
+              >
+              </VideoPane>
             </div>
         </div>
         </div>
